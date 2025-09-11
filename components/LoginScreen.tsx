fe16eacf73c2017ad5c4023b8024a2f03c68d3be
EbrahimAlenezi <ibrahemalwteed@gmail.com>
2025-09-11 23:28:08 +0100

    last update

--- components/LoginScreen.tsx
+++ components/LoginScreen.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 const LoginScreen = () => {
   return (
     <View>
-      <Text>LoginScreen</Text>
+      <Text>LoginScreens</Text>
     </View>
   );
 };
